85b7666ab936851fb4cc23d9883d16e25c28ec89
Samir <122364498+Stileexx@users.noreply.github.com>
2024-03-11 18:46:42 +0100

    here

---
+++ src/Main.java
@@ -0,0 +1,76 @@
+import java.awt.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Syntax of if statements:
+//        if (true/false) {
+//        body
+//        } else {
+////        do this
+//    }
+//
+//        int salary = 25400;
+//
+//        if (salary > 10000) {
+//            salary = salary + 2000;
+//        } else {
+//            salary = salary + 1000;
+//        }
+//
+//        //multiple if-else
+//
+//        if (salary > 10000) {
+//            salary += 2000; //salary = salary + 200;
+//
+//
+//        } else if (salary < 20000) {
+//            salary += 3000;
+//        } else {
+//            salary = salary + 2000;
+//        }
+//
+//        System.out.println(salary);
+////100
+//        for (int num = 1; num <= 5; num++) {
+//            System.out.println(num);
+//
+//        }
+//
+////        q: print number from 1 to 10
+//        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt();
+//        for (int num = 1; num <= n; num++) {
+////            System.out.println(num);
+//            System.out.println(num + " Hello World");
+//        }
+//
+////        while loop:
+////        while (condition) {
+//        //body
+//    }
+
+// while loop when dont know how times it needs to run
+        // for loop if i know how often it need to run
+//        int num = 1;
+//        while (num <= 5) {
+//            System.out.println(num);
+//            num += 1;
+//        }
+
+        //do while
+
+        //do {
+        //this thing
+  //  } while (condition);
+
+//it will execute ATLEAST 1 time, first it reads the body - after this the check
+//
+        int n = 1;
+        do {
+            System.out.println("Hi im executing!");
+
+        } while (n != 1);
+
+    }
+}
