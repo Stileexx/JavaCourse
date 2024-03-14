68a2e756308343eb1f2f5f29793330945d01297c
Samir <122364498+Stileexx@users.noreply.github.com>
2024-03-14 02:16:43 +0100

    Durchschnittberechnung

---
+++ src/Durchschnittberechnung.java
@@ -0,0 +1,12 @@
+public class Durchschnittberechnung {
+
+    public static void main(String[] args) {
+
+        double num1 = 5;
+        double num2 = 2;
+        double num3 = 1;
+
+        double durcchschnitt = (num1 + num2 + num3 / 3);
+        System.out.println("Der durschnitt ist: " + durcchschnitt);
+    }
+}
