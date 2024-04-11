668ec2f1d467dc405a04b954837f47c4119d898c
Samir <122364498+Stileexx@users.noreply.github.com>
2024-04-11 18:20:06 +0200

    internal variable swap

---
+++ src/PassingExample.java
@@ -0,0 +1,11 @@
+public class PassingExample {
+    public static void main(String[] args) {
+        String name = "Samir Abdzl";
+        greet(name);
+
+    }
+
+    static void greet(String naam) {
+        System.out.println(naam);
+    }
+}
