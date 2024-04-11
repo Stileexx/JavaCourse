8bcc979e271f7f278beaf64e03fa89eec3b96960
Samir <122364498+Stileexx@users.noreply.github.com>
2024-04-11 18:14:28 +0200

    Number Swap doesnt work

--- src/NumberSwap.java
+++ src/NumberSwap.java
@@ -5,12 +5,28 @@ public class NumberSwap {
         int a = 10;
         int b = 20;
 
-        int temporary = a;
 
-        a = b;
-        b = temporary;
+//        number swap
+//        int temporary = a;
+//        a = b;
+//        b = temporary;
 
+        swap(a, b);
         System.out.println(a + " " + b);
 
+        String name = "Samir Abdul";
+        changeName(name);
+        System.out.println(name);
+
+    }
+
+     static void changeName(String name) {
+        name = "Gaber Asd";
+    }
+
+    static void swap(int a, int b) {
+        int temporary = a;
+        a = b;
+        b = temporary;
     }
 }
