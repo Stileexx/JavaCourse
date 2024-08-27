40e21be64f0ee970d0f0c75c2a677f9301f847dc
Samir <olyamarkarova13092002@gmail.com>
2024-08-27 13:27:28 +0200

    repetition on if statements and loops

---
+++ src/Basics.java
@@ -0,0 +1,26 @@
+public class Basics {
+
+    public static void main(String[] args) {
+//        int a = 10;
+//        if (a == 12) {
+//            System.out.println("Hello world");
+//        } else {
+//            System.out.println("Incorrect");
+//        }
+
+//        int count = 25;
+
+//        while (count != 10) {
+//            System.out.println(count);
+//        count--;
+//        }
+// When don't know how many times it needs to run ->While loop()
+// If I know how often I use for loop
+//        for loop
+        for (int count = 1; count != 15; count++) {
+            System.out.println(count);
+        }
+
+
+    }
+}
--- src/Durchschnittberechnung.java
+++ src/Durchschnittberechnung.java
@@ -2,11 +2,13 @@ public class Durchschnittberechnung {
 
     public static void main(String[] args) {
 
-        double num1 = 5;
-        double num2 = 2;
-        double num3 = 1;
+        double num1 = 10;
+        double num2 = 5;
+        double num3 = 10;
 
-        double durcchschnitt = (num1 + num2 + num3 / 3);
+        double sum = num1 + num2 + num3;
+
+        double durcchschnitt = (sum / 3);
         System.out.println("Der durschnitt ist: " + durcchschnitt);
     }
 }
